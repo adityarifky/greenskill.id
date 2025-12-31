1feea20ec0d974a6802d7a38e1d95397662ba74f
tools greenskill <greenskilltools@gmail.com>
2025-12-31 03:09:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9692377102-606d5",
+  "appId": "1:159223163006:web:dcf0b06e5d78e45ab7502f",
+  "apiKey": "AIzaSyAEFPFV0_bJ8ydXpF6foLOsygynq3vt18o",
+  "authDomain": "studio-9692377102-606d5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "159223163006"
+};
