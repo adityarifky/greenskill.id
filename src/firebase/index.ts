005fd551559eb5cb44d37960cd79701434f088be
tools greenskill <greenskilltools@gmail.com>
2026-01-01 13:14:11 +0000

    Try fixing this error: `Console Error: [2026-01-01T13:13:33.935Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,7 +3,7 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
@@ -35,17 +35,7 @@ export function initializeFirebase() {
 
 export function getSdks(firebaseApp: FirebaseApp) {
   const firestore = getFirestore(firebaseApp);
-  if (process.env.NODE_ENV === 'development') {
-    try {
-        connectFirestoreEmulator(firestore, 'localhost', 8080);
-    } catch (e) {
-        // Emulator might already be connected
-        if ((e as any).code !== 'failed-precondition') {
-            console.error("Error connecting to Firestore emulator: ", e);
-        }
-    }
-  }
-
+  
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
